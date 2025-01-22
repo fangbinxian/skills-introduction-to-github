34b8aac81439a3c0ea684144bc07bf845fb4f3a3
yfang8 <yfang8@apple.com>
2025-01-22 13:38:04 +0800

    setup the virtual environment

--- first.py
+++ first.py
@@ -1,3 +1,13 @@
-
 # This is an empty Python file
 # This is actually not a python script, just for my learning purpose to be familar with github.
+
+import pyttsx3
+
+def speak_text():
+    engine = pyttsx3.init()
+    text = input("Please enter a few sentences: ")
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak_text()
